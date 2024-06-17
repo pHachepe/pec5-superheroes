b3d4e3533b6bd723f1278c6de264befe8a45660d
PeHachePe <pehachepe@uoc.edu>
2024-06-18 00:38:19 +0200

    add basic comps

---
+++ src/app/components/superhero-detail/superhero-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-superhero-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './superhero-detail.component.html',
+})
+export class SuperheroDetailComponent {
+
+}
---
+++ src/app/components/superhero-list/superhero-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-superhero-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './superhero-list.component.html',
+})
+export class SuperheroListComponent {
+
+}
