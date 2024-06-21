e07143ace29d67555287d51f5fa059fb6569f5a9
PeHachePe <pehachepe@uoc.edu>
2024-06-21 19:34:19 +0200

    add superhero detail

--- src/app/components/superhero-detail/superhero-detail.component.ts
+++ src/app/components/superhero-detail/superhero-detail.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
+import { SuperheroService } from '../../services/superhero.service';
 
 @Component({
   selector: 'app-superhero-detail',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule, MatButtonModule, MatIconModule],
   templateUrl: './superhero-detail.component.html',
 })
 export class SuperheroDetailComponent {
+  superheroService = inject(SuperheroService);
+  route = inject(ActivatedRoute);
+  location = inject(Location);
 
+  superheroId = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+  superhero = toSignal(this.superheroService.getSuperhero(this.superheroId))!;
+  showDetails = signal(false);
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  toggleDetails(): void {
+    this.showDetails.update((value) => !value);
+  }
 }
