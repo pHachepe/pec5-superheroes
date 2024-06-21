e9dd639a3b771aa703d3cc42686e8a8609d3c480
PeHachePe <pehachepe@uoc.edu>
2024-06-21 19:04:34 +0200

    add grid

--- src/app/shared/grid/grid.component.ts
+++ src/app/shared/grid/grid.component.ts
@@ -1,12 +1,23 @@
 import { Component, input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
 import { PagedResponse, Superhero } from '../../models/superhero.model';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, RouterLink],
   templateUrl: './grid.component.html',
 })
 export class GridComponent {
   superheroes = input({ data: [], total: 0 } as PagedResponse<Superhero>);
+
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'image',
+    'race',
+    'height',
+    'weight',
+  ];
 }
